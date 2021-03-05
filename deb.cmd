480ba0d0d6eb5ae204347c2f6be600776197f290
buihien224 <54226198+buihien224@users.noreply.github.com>
2021-03-05 08:10:36 +0700

    Update and rename deb.sh to deb.cmd

---
+++ deb.cmd
@@ -0,0 +1,22 @@
+echo " Clean your phone " ;
+adb shell pm uninstall -k --user 0 com.zhihu.android;
+adb shell pm uninstall -k --user 0 com.xiaomi.jr ;
+adb shell pm uninstall -k --user 0 com.ss.android.article.news;
+adb shell pm uninstall -k --user 0 com.taobao.taobao;
+adb shell pm uninstall -k --user 0 com.duokan.free;
+adb shell pm uninstall -k --user 0 com.dragon.read
+adb shell pm uninstall -k --user 0 com.eg.android.AlipayGphone
+adb shell pm uninstall -k --user 0 com.xiaomi.youpin
+adb shell pm uninstall -k --user 0 com.xiaomi.vipaccount
+adb shell pm uninstall -k --user 0 com.miui.miservice
+adb shell pm uninstall -k --user 0 com.xiaomi.smarthome
+adb shell pm uninstall -k --user 0 com.mipay.wallet
+adb shell pm uninstall -k --user 0 com.xiaomi.gamecenter
+adb shell pm uninstall -k --user 0 com.xiaomi.shop
+adb shell pm uninstall -k --user 0 com.miui.voiceassist
+adb shell pm uninstall -k --user 0 com.android.email
+adb shell pm uninstall -k --user 0 com.miui.huanji
+adb shell pm uninstall -k --user 0 com.miui.virtualsim
+adb shell pm uninstall -k --user 0 com.mi.health
+adb shell pm uninstall -k --user 0 com.android.browser
+echo "done"
