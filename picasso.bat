999f362b85436fbbb7ec67d4af628c7846b5649c
buihien224 <buihien224@gmail.com>
2021-04-10 01:52:21 +0700

    Update picasso.bat
    adding download and install gboard
    remove all chinese keyboard

--- picasso.bat
+++ picasso.bat
@@ -1,13 +1,15 @@
 @echo off
+echo " This script help you clean so many fucking miui trash app"
+echo " feel free to contact me via telegram 035939562"
 cd %cd%/adb
 adb.exe wait-for-device >NUL 2>NUL
 adb.exe devices -l 
 echo " Clean your phone " ;
-adb.exe shell pm uninstall -k --user 0 com.zhihu.android;
-adb.exe shell pm uninstall -k --user 0 com.xiaomi.jr ;
-adb.exe shell pm uninstall -k --user 0 com.ss.android.article.news;
-adb.exe shell pm uninstall -k --user 0 com.taobao.taobao;
-adb.exe shell pm uninstall -k --user 0 com.duokan.free;
+adb.exe shell pm uninstall -k --user 0 com.zhihu.android
+adb.exe shell pm uninstall -k --user 0 com.xiaomi.jr 
+adb.exe shell pm uninstall -k --user 0 com.ss.android.article.news
+adb.exe shell pm uninstall -k --user 0 com.taobao.taobao
+adb.exe shell pm uninstall -k --user 0 com.duokan.free
 adb.exe shell pm uninstall -k --user 0 com.dragon.read
 adb.exe shell pm uninstall -k --user 0 com.eg.android.AlipayGphone
 adb.exe shell pm uninstall -k --user 0 com.xiaomi.youpin
@@ -25,6 +27,16 @@ adb.exe shell pm uninstall -k --user 0 com.mi.health
 adb.exe shell pm uninstall -k --user 0 com.android.browser
 adb.exe shell pm uninstall -k --user 0 com.iflytek.inputmethod.miui
 adb.exe shell pm uninstall -k --user 0 com.baidu.input_mi
+adb.exe shell pm uninstall -k --user 0 com.miui.newhome
+adb.exe shell pm uninstall -k --user 0 com.xiaomi.payment
+adb.exe shell pm uninstall -k --user 0 com.miui.newmidrive
+adb.exe shell pm uninstall -k --user 0 com.mi.live.assistant
+adb.exe shell pm uninstall -k --user 0 com.sohu.inputmethod.sogou.xiaomi
+adb.exe shell pm uninstall -k --user 0 com.miui.smarttravel
+adb.exe shell pm uninstall -k --user 0 com.unionpay.tsmservice.mi
+echo "downloading gboard"
+powershell -Command "Invoke-WebRequest https://download1583.mediafire.com/theszju94f8g/vd16wqk7xddw3p2/Gboard+the+Google+Keyboard_v10.3.05.356487417-release-arm64-v8a_apkpure.com.apk  -Outfile gb.apk"
+adb install gb.apk
 echo "Fuck xiaomi's trash app "
 echo "If you want add more app to delete"
 pause
