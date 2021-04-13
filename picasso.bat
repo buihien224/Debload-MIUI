189434cea8a956b65d9a8c6a03e7229dbf3c5340
buihien224 <buihien224@gmail.com>
2021-04-14 00:10:44 +0700

    test
    deocogidau

--- picasso.bat
+++ picasso.bat
@@ -1,10 +1,36 @@
 @echo off
-echo " This script help you clean so many fucking miui trash app"
-echo " feel free to contact me via telegram 035939562"
+CLS
 cd %cd%/adb
 adb.exe wait-for-device >NUL 2>NUL
 adb.exe devices -l 
-echo " Clean your phone " ;
+echo 1.Clean 
+echo 2.Install
+CHOICE /C 123 /M "Enter your choice:"
+:: Note - list ERRORLEVELS in decreasing order
+IF ERRORLEVEL 1 GOTO Clean
+IF ERRORLEVEL 2 GOTO Install
+:: Install rom 
+:Install
+echo "Go to Recovery"
+adb.exe wait-for-device >NUL 2>NUL
+adb.exe devices
+timeout 15
+echo "Go to MENU (3 lines option)"
+timeout 5
+echo "Go To ADB & Sideload"
+timeout 5
+echo "Swipe to Start Sideload"
+timeout 10
+adb.exe devices
+timeout 2
+set /p UserInputPath=Drag your Zip file ROM, Magisk To CMD
+adb.exe sideload %UserInputPath%
+echo DONE
+GOTO End
+:Clean
+echo " This script help you clean so many fucking miui trash app"
+echo " feel free to contact me via telegram 035939562"
+echo " Clean your phone " 
 adb.exe shell pm uninstall -k --user 0 com.zhihu.android
 adb.exe shell pm uninstall -k --user 0 com.xiaomi.jr 
 adb.exe shell pm uninstall -k --user 0 com.ss.android.article.news
@@ -37,9 +63,8 @@ adb.exe shell pm uninstall -k --user 0 com.unionpay.tsmservice.mi
 echo "downloading gboard"
 powershell -Command "Invoke-WebRequest https://download1583.mediafire.com/theszju94f8g/vd16wqk7xddw3p2/Gboard+the+Google+Keyboard_v10.3.05.356487417-release-arm64-v8a_apkpure.com.apk  -Outfile gb.apk"
 adb install gb.apk
+echo " pls allow to install in your phone"
 echo "Fuck xiaomi's trash app "
 echo "If you want add more app to delete"
-pause
-start https://docs.google.com/spreadsheets/d/1uW2hyxLQRuGcxVtWkJ03GBfJaMLoJngA_iV328xk940/edit?usp=sharing
-cd ..
+GOTO End
 pause
