e66d844019477641e09b598440da8077682ea8e4
buihien224 <buihien224@gmail.com>
2021-04-14 00:21:46 +0700

    Update picasso.bat

--- picasso.bat
+++ picasso.bat
@@ -1,25 +1,26 @@
 @echo off
 CLS
 cd %cd%/adb
+echo wait-for-device.....
 adb.exe wait-for-device >NUL 2>NUL
 adb.exe devices -l 
 echo 1.Install 
 echo 2.Clean
-CHOICE /C 12 /M "Enter your choice:"
+CHOICE /C 12 /M Enter your choice:
 :: Note - list ERRORLEVELS in decreasing order
 IF ERRORLEVEL 2 GOTO Clean
 IF ERRORLEVEL 1 GOTO Install
 :: Install rom 
 :Install
-echo "Auto enter to recovery"
+echo Auto enter to recovery
 adb.exe reboot recovery
 adb.exe devices
 timeout 5
-echo "Go to MENU (3 lines option)"
+echo Go to MENU (3 lines option)
 timeout 3
-echo "Go To ADB & Sideload"
+echo Go To ADB & Sideload
 timeout 3
-echo "Swipe to Start Sideload"
+echo Swipe to Start Sideload
 timeout 5
 adb.exe devices
 timeout 2
@@ -29,9 +30,9 @@ echo DONE
 GOTO End
 
 :Clean
-echo " This script help you clean so many fucking miui trash app"
-echo " feel free to contact me via telegram 035939562"
-echo " Clean your phone " 
+echo  This script help you clean so many fucking miui trash app
+echo  feel free to contact me via telegram 035939562
+echo  Clean your phone  
 adb.exe shell pm uninstall -k --user 0 com.zhihu.android
 adb.exe shell pm uninstall -k --user 0 com.xiaomi.jr 
 adb.exe shell pm uninstall -k --user 0 com.ss.android.article.news
@@ -61,12 +62,12 @@ adb.exe shell pm uninstall -k --user 0 com.mi.live.assistant
 adb.exe shell pm uninstall -k --user 0 com.sohu.inputmethod.sogou.xiaomi
 adb.exe shell pm uninstall -k --user 0 com.miui.smarttravel
 adb.exe shell pm uninstall -k --user 0 com.unionpay.tsmservice.mi
-echo "downloading gboard"
-powershell -Command "Invoke-WebRequest https://download1583.mediafire.com/theszju94f8g/vd16wqk7xddw3p2/Gboard+the+Google+Keyboard_v10.3.05.356487417-release-arm64-v8a_apkpure.com.apk  -Outfile gb.apk"
+echo downloading gboard
+powershell -Command Invoke-WebRequest https://download1583.mediafire.com/theszju94f8g/vd16wqk7xddw3p2/Gboard+the+Google+Keyboard_v10.3.05.356487417-release-arm64-v8a_apkpure.com.apk  -Outfile gb.apk
 adb install gb.apk
-echo " pls allow to install in your phone"
-echo "Fuck xiaomi's trash app "
-echo "If you want add more app to delete"
+echo  pls allow to install in your phone
+echo Fuck xiaomi's trash app 
+echo If you want add more app to delete
 GOTO End
 
 End 
