6551f4bf09408ba37035c89e39e67e07324aa8e2
buihien224 <buihien224@gmail.com>
2021-04-15 12:27:11 +0700

    Update picasso.bat

--- picasso.bat
+++ picasso.bat
@@ -1,29 +1,20 @@
-echo off
-cd %cd%/adb
-:begin
+@echo off
+CLS
 cd %cd%/adb
 echo wait-for-device.....
-adb.exe wait-for-device
+adb.exe wait-for-device >NUL 2>NUL
 adb.exe devices -l 
-echo Select a task:
-echo =============
-echo -
-echo 1) INSTALL
-echo 2) CLEAN
-echo -
-set /p op=Type option:
-if "%op%"=="1" goto op1
-if "%op%"=="2" goto op2
-
-echo Please Pick an option:
-goto begin
-
-
-:op1
-CLS
+echo 1.Install 
+echo 2.Clean
+echo 3.GBoard
+CHOICE /C 12 /M "Enter your choice:"
+:: Note - list ERRORLEVELS in decreasing order
+IF ERRORLEVEL 3 GOTO GBoard
+IF ERRORLEVEL 2 GOTO Clean
+IF ERRORLEVEL 1 GOTO Install
+:: Install rom 
+:Install
 echo Auto enter to recovery
-adb.exe reboot recovery
-echo rebooting .......
 adb.exe wait-for-recovery
 CLS
 echo ##################################
@@ -34,17 +25,37 @@ echo #    Swipe to Start Sideload     #
 echo ##################################
 adb.exe wait-for-sideload
 set /p UserInputPath=Drag your Zip file ROM, Magisk To CMD : 
-echo FLASING                                                                                                 
+echo #######################################
+echo FFFFFFF LL        AAA    SSSSS  HH   HH
+echo FF      LL       AAAAA  SS      HH   HH
+echo FFFF    LL      AA   AA  SSSSS  HHHHHHH
+echo FF      LL      AAAAAAA      SS HH   HH
+echo FF      LLLLLLL AA   AA  SSSSS  HH   HH   
+echo #######################################                                                                                            
 adb.exe sideload %UserInputPath%
 adb.exe wait-for-recovery 
 CLS
-echo DONE  
+echo ###############################
+echo DDDDD    OOOOO  NN   NN EEEEEEE
+echo DD  DD  OO   OO NNN  NN EE     
+echo DD   DD OO   OO NN N NN EEEEE  
+echo DD   DD OO   OO NN  NNN EE     
+echo DDDDDD   OOOO0  NN   NN EEEEEEE
+echo ###############################
 adb.exe reboot
-goto begin
+pause
+GOTO End
 
-:op2
-CLS
-echo cleaning your phone
+
+:Clean
+echo #######################################
+echo  CCCCC  LL      EEEEEEE   AAA   NN   NN
+echo CC    C LL      EE       AAAAA  NNN  NN
+echo CC      LL      EEEEE   AA   AA NN N NN
+echo CC    C LL      EE      AAAAAAA NN  NNN
+echo  CCCCC  LLLLLLL EEEEEEE AA   AA NN   NN
+echo #######################################
+echo.
 adb.exe shell pm uninstall -k --user 0 com.zhihu.android
 adb.exe shell pm uninstall -k --user 0 com.xiaomi.jr 
 adb.exe shell pm uninstall -k --user 0 com.ss.android.article.news
@@ -74,13 +85,21 @@ adb.exe shell pm uninstall -k --user 0 com.mi.live.assistant
 adb.exe shell pm uninstall -k --user 0 com.sohu.inputmethod.sogou.xiaomi
 adb.exe shell pm uninstall -k --user 0 com.miui.smarttravel
 adb.exe shell pm uninstall -k --user 0 com.unionpay.tsmservice.mi
+echo ###############################
+echo DDDDD    OOOOO  NN   NN EEEEEEE
+echo DD  DD  OO   OO NNN  NN EE     
+echo DD   DD OO   OO NN N NN EEEEE  
+echo DD   DD OO   OO NN  NNN EE     
+echo DDDDDD   OOOO0  NN   NN EEEEEEE
+echo ###############################  
+pause
+GOTO End
+
+:GBoard
 echo downloading gboard
 powershell -Command "Invoke-WebRequest https://download1583.mediafire.com/theszju94f8g/vd16wqk7xddw3p2/Gboard+the+Google+Keyboard_v10.3.05.356487417-release-arm64-v8a_apkpure.com.apk  -Outfile gb.apk"
 adb install gb.apk
-CLS
-echo DONE
-pause
-goto begin
+GOTO End
 
-:exit
-@exit
+
+End 
