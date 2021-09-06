1fe031331ae42543d0fc499e0a98a8c76be4b7b7
buihien224 <54226198+buihien224@users.noreply.github.com>
2021-09-06 22:10:15 +0700

    Test
    Test termux

---
+++ deboat.sh
@@ -0,0 +1,34 @@
+@echo off
+echo " This script help you clean so many fucking miui trash app"
+echo " feel free to contact me via telegram 035939562"
+echo " Clean your phone " 
+pm uninstall -k --user 0 com.zhihu.android
+pm uninstall -k --user 0 com.xiaomi.jr 
+pm uninstall -k --user 0 com.ss.android.article.news
+pm uninstall -k --user 0 com.taobao.taobao
+pm uninstall -k --user 0 com.duokan.free
+pm uninstall -k --user 0 com.dragon.read
+pm uninstall -k --user 0 com.eg.android.AlipayGphone
+pm uninstall -k --user 0 com.xiaomi.youpin
+pm uninstall -k --user 0 com.xiaomi.vipaccount
+pm uninstall -k --user 0 com.miui.miservice
+pm uninstall -k --user 0 com.xiaomi.smarthome
+pm uninstall -k --user 0 com.mipay.wallet
+pm uninstall -k --user 0 com.xiaomi.gamecenter
+pm uninstall -k --user 0 com.xiaomi.shop
+pm uninstall -k --user 0 com.miui.voiceassist
+pm uninstall -k --user 0 com.android.email
+pm uninstall -k --user 0 com.miui.huanji
+pm uninstall -k --user 0 com.miui.virtualsim
+pm uninstall -k --user 0 com.mi.health
+pm uninstall -k --user 0 com.android.browser
+pm uninstall -k --user 0 com.iflytek.inputmethod.miui
+pm uninstall -k --user 0 com.baidu.input_mi
+pm uninstall -k --user 0 com.miui.newhome
+pm uninstall -k --user 0 com.xiaomi.payment
+pm uninstall -k --user 0 com.miui.newmidrive
+pm uninstall -k --user 0 com.mi.live.assistant
+pm uninstall -k --user 0 com.miui.smarttravel
+pm uninstall -k --user 0 com.unionpay.tsmservice.mi
+echo "Fuck xiaomi's trash app "
+echo done
